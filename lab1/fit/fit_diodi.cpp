52130ce8895e3b44395769b413c69748a35b5260
A-lida <alida.castagnoli5@studio.unibo.it>
2024-11-15 16:03:36 +0100

    Analisi dati + immagini fatta

--- lab1/fit/fit_diodi.cpp
+++ lab1/fit/fit_diodi.cpp
@@ -1,66 +1,99 @@
 #include "TCanvas.h"
 #include "TF1.h"
 #include "TGraphErrors.h"
+#include "TPaveStats.h"
 #include "TROOT.h"
 #include "TStyle.h"
 #include <Rtypes.h>
 #include <TMath.h>
+#include <TPad.h>
+#include <TVirtualPad.h>
+#include <cstring>
 
-inline void setStyle() {
+void setStyle() {
   //   gROOT->SetStyle("Plain");
   gStyle->SetOptStat(2210);
-  gStyle->SetOptFit(111);
+  gStyle->SetOptFit();
   gStyle->SetPalette(57);
   gStyle->SetOptTitle(0);
 }
 
-int main() {
-  setStyle();
+void cutiefier(TVirtualPad* pad) {
+  gPad->Update();
+  TPaveStats *statbox = (TPaveStats *)pad->GetPrimitive("stats");
 
-  TCanvas c_s{"", "", 1920, 1080};
-  c_s.Divide(2);
-  // c_s.GetPad(1)->SetLogy();
-  TGraphErrors graph_s_log("../fit/diodo_silicio.csv");
-  graph_s_log.SetTitle(
-      "Caratteristica corrente-tensione;Tensione (mV);Corrente (mA)");
+  if (statbox) {
+    // Cambia le coordinate NDC (x1, y1, x2, y2)
+    statbox->SetX1NDC(0.486422 ); // Nuova posizione X1 (vicino al bordo sinistro)
+    statbox->SetX2NDC(0.986075); // Nuova posizione X2
+    statbox->SetY1NDC( 0.840136); // Altezza inferiore
+    statbox->SetY2NDC(0.944496); // Altezza superiore
+    gPad->Modified();
+  } else {
+    std::cerr << "Statbox non trovata!\n";
+  }
+}
 
-  auto n_s = graph_s_log.GetN();
-  auto x_s = graph_s_log.GetX();
-  auto y_s = graph_s_log.GetY();
+void create_graphs(const char *source_file, const char *title, const char* output_file) {
+  TCanvas c{"", "", 1920, 1080};
+  c.Divide(2);
 
-  for (int i = 0; i < n_s; i++) {
-    y_s[i] = TMath::Log(y_s[i]);
+  TGraphErrors graph(source_file);
+  graph.SetTitle(title);
+
+  c.cd(1);
+
+  TF1 exp("esponenziale", "[0]*exp(x/[1])");
+  exp.SetParameters(1E-6, 350. / 6);
+  exp.SetParNames("I_{0}", "#eta V_{D}");
+  exp.SetLineColor(kOrange);
+
+  graph.SetMarkerSize(1);
+  graph.SetMarkerStyle(1);
+  graph.Fit(&exp);
+
+  graph.Draw("AP*");
+  c.Draw();
+  cutiefier(c.GetPad(1));
+
+  auto N = graph.GetN();
+  auto y = new double[N];
+  std::memcpy(y, graph.GetY(), N * sizeof(double));
+  auto err_y = new double[N];
+  std::memcpy(err_y, graph.GetEY(), N * sizeof(double));
+
+  for (int i = 0; i < N; i++) {
+    err_y[i] = 1. / y[i] * err_y[i];
+    y[i] = TMath::Log(y[i]);
   }
 
-  TGraphErrors graph_s{n_s, x_s, y_s, graph_s_log.GetEX(), graph_s_log.GetEY()};
+  TGraphErrors graph_log{N, graph.GetX(), y, graph.GetEX(), err_y};
 
-  TF1 retta("caratteristica", "[0] * x + [1]");
-  retta.SetParameter(0, 1. / 420);
-  retta.SetParameter(1, -5);
+  TF1 retta("caratteristica", "[0]+ x/ [1]");
+  retta.SetParameters(-3, 1. / 50);
+  retta.SetParNames("ln(I_{0})", "#eta V_{D}");
   retta.SetLineColor(kBlue);
 
-  graph_s.SetMarkerStyle(1);
-  graph_s.SetMarkerSize(1);
-  graph_s.Fit(&retta);
+  graph_log.SetTitle(title);
+  graph_log.SetMarkerStyle(1);
+  graph_log.SetMarkerSize(1);
+  graph_log.Fit(&retta);
 
-  // TF1 exp("esponenziale", "[0]*exp(-x/[1])");
-  // exp.SetParameters(TMath::Exp(-5), 1. / 420);
-  // exp.SetLineColor(kOrange);
+  c.cd(2);
+  graph_log.Draw("AP*");
+  c.Draw();
+  cutiefier(c.GetPad(2));
 
-  // graph_s_log.SetMarkerSize(1);
-  // graph_s_log.SetMarkerStyle(1);
-  // graph_s_log.Fit(&exp);
+  c.SaveAs(output_file);
+}
 
-  c_s.cd(1);
-  graph_s_log.Draw("AP");
+int main() {
+  // void fit_diodi() {
+  setStyle();
 
-  c_s.cd(2);
+  create_graphs("../fit/diodo_silicio.csv",
+                "Caratteristica corrente-tensione silicio;Tensione (mV);Corrente (mA)", "silicio.root");
 
-  graph_s.Draw("AP");
-  c_s.SaveAs("silicio.png");
-
-  // TCanvas c_g{};
-  // c_g.GetPad(1)->SetLogy();
-  // TGraphErrors graph_g;
-  // TGraphErrors graph_g_log("../fit/diodo_germanio.csv");
+  create_graphs("../fit/diodo_germanio.csv",
+                "Caratteristica corrente-tensione germanio;Tensione (mV);Corrente (mA)", "germanio.root");
 }
