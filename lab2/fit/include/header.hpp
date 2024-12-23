a5c3c60e1052d9d753b56880cf2de8cfde8ff6b9
Pe13 <paolo.peforni@gmail.com>
2024-12-23 16:46:53 +0100

    Creata la procedura di fit.

---
+++ lab2/fit/include/header.hpp
@@ -0,0 +1,17 @@
+//
+// Created by paolo on 23/12/2024.
+//
+
+#ifndef HEADER_HPP
+#define HEADER_HPP
+
+#include <vector>
+
+struct GraphInitializers {
+  std::vector<double> X{};
+  std::vector<double> Y{};
+  std::vector<double> EX{};
+  std::vector<double> EY{};
+};
+
+#endif //HEADER_HPP
