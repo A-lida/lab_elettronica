cf0ad942ec2075c5ca1cdd4d82c7bc14c180909d
paolo <paolo.peforni@gmail.com>
2024-12-24 02:03:03 +0100

    Un solo grafico con dati fittati e non

--- lab2/fit/fit_caratteristica.cpp
+++ lab2/fit/fit_caratteristica.cpp
@@ -42,7 +42,7 @@ void filterData(const GraphInitializers &allData, GraphInitializers &fit,
             << " elements\n\n";
 }
 
-void create_graphs(const char *source_file, const char *title,
+void create_graph(const char *source_file, const char *title,
                    const double minX, const double maxX,
                    const char *output_file) {
   GraphInitializers allData{};
@@ -54,54 +54,47 @@ void create_graphs(const char *source_file, const char *title,
   filterData(allData, fit, noFit, minX, maxX);
 
   // Creo la canvas
-  TCanvas c{"", "", 1280, 1080};
-  c.Divide(2);
+  TCanvas c{"", "", 1600, 1080};
+  c.Divide(1);
 
   // Creo i grafici
-  TGraphErrors dummyFitGraph{static_cast<Int_t>(fit.X.size()), fit.X.data(),
+  TGraphErrors fitGraph{static_cast<Int_t>(fit.X.size()), fit.X.data(),
                              fit.Y.data(), fit.EX.data(), fit.EY.data()};
   TGraphErrors noFitGraph{static_cast<Int_t>(noFit.X.size()), noFit.X.data(),
                           noFit.Y.data(), noFit.EX.data(), noFit.EY.data()};
 
   // Configuro gli stili dei grafici
-  dummyFitGraph.SetMarkerStyle(1); // Style
+  fitGraph.SetMarkerStyle(1); // Style
   noFitGraph.SetMarkerStyle(1);
 
-  dummyFitGraph.SetMarkerSize(1); // Size
+  fitGraph.SetMarkerSize(1); // Size
   noFitGraph.SetMarkerSize(1);
 
-  dummyFitGraph.SetMarkerColor(kBlue); // Color
+  fitGraph.SetMarkerColor(kBlue); // Color
   noFitGraph.SetMarkerColor(kRed);
 
   noFitGraph.SetTitle(title); // Title
 
-  // Disegno il grafico totale
-  c.cd(1);
-  noFitGraph.Draw("AP*");
-  dummyFitGraph.Draw("SAME P*");
-
-  // Genero il grafico del fit
-  auto fitGraph = dynamic_cast<TGraphErrors *>(dummyFitGraph.Clone());
-  TF1 retta("caratteristica", "[0] + x * [1]");
+  // Creo la funzione di fit
+  TF1 retta("caratteristica", "[0] + x * [1]", minX, maxX);
   retta.SetParameters(1, .5);
   // FIXME V_{A} sarebbe l'intercetta con l'asse X, non so come chiamare
   //  l'intercetta con l'asse Y
   retta.SetParNames("V_{A}", "g");
   retta.SetLineColor(kBlue);
 
-  fitGraph->Fit(&retta);
+  // Fitto con il range della funzione
+  fitGraph.Fit(&retta, "R");
 
-  // Disegno il grafico del fit
-  c.cd(2);
-  fitGraph->Draw("AP*");
+  // Disegno il grafico totale
+  c.cd(1);
+  noFitGraph.Draw("AP*");
+  fitGraph.Draw("SAME P*");
   c.Draw();
-  cutiefier(c.GetPad(2));
+  cutiefier(c.GetPad(1));
 
   // Salvo il risultato
   c.SaveAs(output_file);
-
-  // Cleanup
-  delete fitGraph;
 }
 
 int main() {
@@ -109,13 +102,13 @@ int main() {
   // FIXME Aggiustare i titoli (così fanno un po' schifo)
   // TODO Mettere i titoli anche ai grafici dei fit (dopo [23/12] lo faccio io
   //  [Paolo])
-  create_graphs("../dati_100uA.csv",
+  create_graph("../dati_100uA.csv",
                 "Caratteristica corrente di collettore - tensione con corrente "
                 "di base 100 #muA;Tensione (mV);Corrente (mA)",
                 0.9, 3.3, "fit_100uA.root");
 
   // TODO Testare con Chauvenier se il dato a 1.5 è rigettabile
-  create_graphs("../dati_200uA.csv",
+  create_graph("../dati_200uA.csv",
                 "Caratteristica corrente di collettore - tensione con corrente "
                 "di base 200 #muA;Tensione (mV);Corrente (mA)",
                 1.02, 3.1, "fit_200uA.root");
